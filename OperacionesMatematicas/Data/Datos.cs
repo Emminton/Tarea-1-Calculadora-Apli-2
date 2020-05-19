b48af0664f73b62850f06ab7280df840bd7414d6
Emminton Manuel Ure�a Santana <emminton_urena@ucne.edu.do>
2020-05-19 16:29:43 -0400

    Tarea 1 Restar, Multiplicar y Sumar

---
+++ OperacionesMatematicas/Data/Datos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OperacionesMatematicas.Data
+{
+    public class Datos
+    {
+        [Required(ErrorMessage = "El campo no puede estar vacío.")]
+        public  decimal numero1 { get; set; }
+        [Required(ErrorMessage = "El campo no puede estar vacío.")]
+        public decimal numero2 { get; set; }
+        public decimal resultado { get; set; }
+
+        public Datos()
+        {
+            numero1 = 0;
+            numero2 = 0;
+            resultado = 0;
+        }
+    }
+}
